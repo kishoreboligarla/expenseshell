6baf0dfd2a7fc6cf520bd3536c720cfc9aaa6822
kishore <kishoreboligarla@gmail.com>
2024-11-20 02:18:33 +0530

    shell

--- backend.sh
+++ backend.sh
@@ -45,7 +45,7 @@ fi
 mkdir -p /app #here -p is used for check the app direcory create or not if it creates goes next step else create a app directory
 VALIDATE $? "creating /app folder" 
 curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip  &>>$LOG_FILE
-VALIDATE $? "downloading backend appilication code" #| tee -a $LOG_FILE
+VALIDATE $? "downloading backend appilication code" | tee -a $LOG_FILE
 cd /app
 unzip /tmp/backend.zip &>>$LOG_FILE
 VALIDATE $? "extracting backend application code"
